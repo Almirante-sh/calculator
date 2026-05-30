c7026727bea8be04384753689e72ebce63d1ad9a
Almirante <Almirante.xo@gmail.com>
2026-05-30 19:07:39 +0000

    Fixed bug

--- calculator.cpp
+++ calculator.cpp
@@ -5,30 +5,34 @@ double number1;
 double number2;
 double result;
 char userchoice;
+//
 std::cout << "Enter your operation (+,-,*,/)\n>";
 std::cin >> userchoice;
 std::cout << "Enter your first number:\n>";
 std::cin >> number1;
 std::cout << "Enter your second number:\n>";
-std::cin >> number2;
+std::cin >> number2;  
 if (userchoice == '+') {
-  result = number1 + number2;
+    result = number1 + number2;
 }
-else if (userchoice == '-'){
-   result = number1 - number2;
+else if (userchoice == '-') {
+    result = number1 - number2;
 } 
-else if (userchoice == '*'){
-   result = number1 * number2;
+else if (userchoice == '*') {
+    result = number1 * number2;
 }
-else if (userchoice == '/'){
-   result = number1 / number2;
+else if (userchoice == '/') {
+  if (number2 == 0 ) {
+    std::cout << "ERROR: You can't divide by 0\n";
+    return 1;
 }
-if (number2 == 0 ){
-std::cout << "ERROR: you can't divide by 0\n";
+    result = number1 / number2;
 }
 else{
-std::cout << "ERROR: invalid operation\n";
+std::cout << "ERROR: Invalid operation\n";
+return 1;
 }
 std::cout << "Result:" << result << "\n";
 return 0;
 }
+
